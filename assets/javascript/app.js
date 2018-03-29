a98e0bc7071543279cd9b3d6b7297a4850619832
Shannon Doepke <shannondoepke@Shannons-MacBook-Air.local>
2018-03-29 13:22:22 -0400

    Update object constructor logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,16 +9,21 @@ var weather = {
 function weather(city){
 
 };
+//Create vacations array to push new vacations
 
+//Push activities to vacation
+
+//Create object contructor for vacation
 function Vacation (name, location) {
-    this.name = name;
-    this.location=location;
-    this.activities=[];
+    this.name = name,
+    this.location = location,
+    this.activities = [],
     this.addActivity = function (activity){
         this.activities.push(activity);
     }
 };
 
+//Create object constructor for activity
 function Activity (location, date, description, completed) {
     this.location = location;
     this.date = date;
@@ -26,9 +31,27 @@ function Activity (location, date, description, completed) {
     this.completed = completed
 }
 
+//Placeholder for new vacation name and location
 var vacation = new Vacation("Paris", "Paris");
 
+//Console log vacation
 console.log(vacation);
 
-$("#add-button").click()
+//Create function that will clear entry fields upon clicking submit
+// function clear() {
+//     $("#date").empty();
+//     $("#activity").empty();
+//     $("#conf-number").empty();
+//   }
+
+// //Create click event function for entry form
+// $("#add-button").on("click", function(event){
+
+// var dateEntry = $("#date").val().trim();
+// var activityEntry = $("#activity").val().trim();
+
+
+
+
+// });
 
