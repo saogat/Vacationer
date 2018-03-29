9f5e3ea874354b2d917250b9b67502aa419883c4
Saogat Rab <saogat.rab@icloud.com>
2018-03-29 13:31:36 -0400

    added weather api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 
-$(".tab").on("click", function(event){
+var weatherData = [];
+
+$(".tab").on("click", function (event) {
     event.preventDefault();
 
     console.log("clicked on vacation city tab");
@@ -10,40 +12,78 @@ $(".tab").on("click", function(event){
 });
 
 //add vacation tab
-function addVacation(){
-    
+function addVacation() {
+
 }
 
 //add activity to the vacation 
-function addActivity(){
+function addActivity() {
 
     $("#to-dos").append($("<p>").text("test"));
-        
-       // ("<p>Checkin into hotel</p>");
+
+    // ("<p>Checkin into hotel</p>");
     console.log("adding activity");
 
 };
 
 
-//get map from Google API
-function getMap(){
-$.ajax({
-    url: url,
-    method: 'GET',
-}).done(function (response) {
+function Weather(location, temperature, min, max, humidity, description) {
+    this.location = location;
+    this.temperature = temperature;
+    this.min = min;
+    this.max = max;
+    this.humidity = humidity;
+    this.description = description;
+}
 
-});
-
-};
-
-
-//get weather from Weather API
-function getWeather(){
+ //get weather from Weather API
+var getWeather = function (location) {
+    var url = "http://api.openweathermap.org/data/2.5/forecast?";
+    url += "APPID=e059918f7e48a37962d40029f4db4443";
+    url += "&q=";
+    url += location;
+    url += "&units=imperial";
     $.ajax({
         url: url,
         method: 'GET',
     }).done(function (response) {
-    
+        
+        for(var i=0; i<5; i++){
+            var eachWeatherData = response.list[i+3];
+            var weather = new Weather(
+                location, 
+                eachWeatherData.main.temp, 
+                eachWeatherData.main.temp_min,
+                eachWeatherData.main.temp_max,
+                eachWeatherData.main.humidity,
+                eachWeatherData.weather[0].description);
+                weatherData.push(weather);
+        }
+
+        // response.list.forEach(function(eachWeatherData){
+        //     var weather = new Weather(
+        //         location, 
+        //         eachWeatherData.main.temp, 
+        //         eachWeatherData.main.temp_min,
+        //         eachWeatherData.main.temp_max,
+        //         eachWeatherData.main.humidity,
+        //         eachWeatherData.weather[0].description);
+        //         weatherData.push(weather);
+        // });
     });
-    
-    };
+};
+
+getWeather("London,UK");
+console.log(weatherData);
+
+//get map from Google API
+function getMap() {
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function (response) {
+
+    });
+
+};
+
