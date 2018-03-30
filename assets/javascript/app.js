7e4e97cc280a3f69b7f15db29056f98464050650
Siyan Zhou <siyanzhou@Siyans-MBP.lan>
2018-03-30 01:32:18 -0400

    google map feature added to the website

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 var geoLocation;
+var map;
 
 //Global variable object
 function Vacationer(name, password, vacations, selectedVacation) {
@@ -170,114 +171,109 @@ $("#to-do-list").on("click", ".checkbox", function () {
 
 
 //get map from Google API
-function DisplayMap(city) {
-    this.geocoding = function () {
-        //whatever the name is from the button that has been clicked
-        var geocodingRequest = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyD6ro8ednBUBnAeYhcjizzp3NvEqFCScNs";
-        $.ajax({
-            url: geocodingRequest,
-            method: 'GET',
-        }).then(function (response) {
-            console.log(response)
-            geoLocation = response.results[0].geometry.location;
-            console.log(geoLocation)
-        });
+
+function geoCoding(city) {
+    //whatever the name is from the button that has been clicked
+    var geocodingRequest = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyD6ro8ednBUBnAeYhcjizzp3NvEqFCScNs";
+    $.ajax({
+        url: geocodingRequest,
+        method: 'GET',
+    }).then(function (response) {
+        geoLocation = response.results[0].geometry.location;
+        console.log(geoLocation);
+        mapSetCenter(geoLocation);
+    });
+}
+
+$(document).on("click", ".tab", function () {
+    geoCoding("London");
+});
+
+function mapSetCenter() {
+
+    if (map !== undefined && typeof map === "object") {
+        map.setCenter(geoLocation);
+    } else {
+        mapSetCenter();
     }
 
+    // or we can run this  checkLoop:
 
-    // this.mapDisplay = function(){
-    //     function initMap() {
-    //         var map = new google.maps.Map(document.getElementById('map'), {
-    //           mapTypeControl: false,
-    //           center: geoLocation,
-    //           zoom: 13
-    //         });
-
-    //         new AutocompleteDirectionsHandler(map);
-    //       }
-
-
-    //       function AutocompleteDirectionsHandler(map) {
-    //         this.map = map;
-    //         this.originPlaceId = null;
-    //         this.destinationPlaceId = null;
-    //         this.travelMode = 'WALKING';
-    //         var originInput = document.getElementById('origin-input');
-    //         var destinationInput = document.getElementById('destination-input');
-    //         var modeSelector = document.getElementById('mode-selector');
-    //         this.directionsService = new google.maps.DirectionsService;
-    //         this.directionsDisplay = new google.maps.DirectionsRenderer;
-    //         this.directionsDisplay.setMap(map);
-
-    //         var originAutocomplete = new google.maps.places.Autocomplete(
-    //             originInput, {placeIdOnly: true});
-    //         var destinationAutocomplete = new google.maps.places.Autocomplete(
-    //             destinationInput, {placeIdOnly: true});
-
-    //         this.setupClickListener('changemode-walking', 'WALKING');
-    //         this.setupClickListener('changemode-transit', 'TRANSIT');
-    //         this.setupClickListener('changemode-driving', 'DRIVING');
-
-    //         this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-    //         this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-    //         this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-    //         this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-    //         this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-    //       }
-
-    //       // Sets a listener on a radio button to change the filter type on Places
-    //       // Autocomplete.
-    //       AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
-    //         var radioButton = document.getElementById(id);
-    //         var me = this;
-    //         radioButton.addEventListener('click', function() {
-    //           me.travelMode = mode;
-    //           me.route();
-    //         });
-    //       };
-
-    //       AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
-    //         var me = this;
-    //         autocomplete.bindTo('bounds', this.map);
-    //         autocomplete.addListener('place_changed', function() {
-    //           var place = autocomplete.getPlace();
-    //           if (!place.place_id) {
-    //             window.alert("Please select an option from the dropdown list.");
-    //             return;
-    //           }
-    //           if (mode === 'ORIG') {
-    //             me.originPlaceId = place.place_id;
-    //           } else {
-    //             me.destinationPlaceId = place.place_id;
-    //           }
-    //           me.route();
-    //         });
-
-    //       }
-    //     }
-
-    //       AutocompleteDirectionsHandler.prototype.route = function() {
-    //         if (!this.originPlaceId || !this.destinationPlaceId) {
-    //           return;
-    //         }
-    //         var me = this;
-
-    //         this.directionsService.route({
-    //           origin: {'placeId': this.originPlaceId},
-    //           destination: {'placeId': this.destinationPlaceId},
-    //           travelMode: this.travelMode
-    //         }, function(response, status) {
-    //           if (status === 'OK') {
-    //             me.directionsDisplay.setDirections(response);
-    //           } else {
-    //             window.alert('Directions request failed due to ' + status);
-    //           }
-    //         });
-    //       };
-
+    // try {
+    //     map.setCenter(geoLocation);
+    // } catch (error) {
+    //     console.log(error);
+    //     mapSetCenter();
     // }
 
 }
 
-var London = new DisplayMap("London").geocoding();
+function initAutocomplete() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 33.7489954, lng: -84.3879824 },
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+    console.log(map);
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
+
+
+
