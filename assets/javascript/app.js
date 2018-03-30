e881cf508ff0e1788aeeb3aeb2a325dba0e55421
Saogat Rab <saogat.rab@icloud.com>
2018-03-30 12:02:42 -0400

    added google login api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -258,6 +258,15 @@ $("#to-do-list").on("click", ".checkbox", function () {
 })
 
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+
 //get map from Google API
 
 function geoCoding(city) {
@@ -276,6 +285,7 @@ function geoCoding(city) {
 $(document).on("click", ".tab", function () {
     geoCoding(newCity);
 });
+
 
 function mapSetCenter() {
 
