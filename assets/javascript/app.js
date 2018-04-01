706a923f8d3b32401d681da9e91498eb08d0aed4
Shannon Doepke <shannondoepke@Shannons-Air.lan>
2018-04-01 16:23:44 -0400

    Add Pixbay image API. Update background images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -387,3 +387,21 @@ function initAutocomplete() {
     });
 	
 }
+
+//Pixabay Photo API
+var pixURL = "https://pixabay.com/api/?q=Paris&image_type=photo&category=places&orientation=vertical&safesearch=true&order=popular&key=8561959-695370e3d9d8574348bbe6f72"
+
+$.ajax({
+    url: pixURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    // for(i = 0; i < response.hits.length; i++){
+        var imageURL = response.hits[0].webformatURL;
+        console.log("ImageURL " + imageURL);
+        newImage = $("<img>");
+        $(newImage).attr("src", imageURL);
+        $(newImage).attr("width", 500)
+        $("#photo-feed").append(newImage);
+    });
