6201a1f325fe62bda7a365410cdb5946d372078f
Gary Estime <visionzbg@yahoo.com>
2018-04-03 15:23:23 -0400

    Updated Google sign in text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -118,7 +118,7 @@ function onSignIn(googleUser) {
     // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var userDiv = $(".user");
     var userImage = $("<img>").attr("src", profile.getImageUrl());
-    var userName = $("<p>").text(profile.getName());
+    var userName = $("<p>").attr("id", "signon-id").text(profile.getName());
     userDiv.empty();
     userDiv.append(userImage);
     userDiv.append(userName);
